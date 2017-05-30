1f36bbb47678c89f625dad6d7ce31aae31b3fb93
Ilya <Ilya>
2017-05-30 14:20:05 +0400

    [123]

---
+++ UT/TrySquare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTest
+{
+    public class Trys
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void world()
+        {
+            Console.WriteLine("HEllo_motherfucka");
+        }
+
+        public int square(int x)
+        {
+            return x*x;
+        }
+
+    }
+}
---
+++ Unittest1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UTest;
+
+namespace Unittest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Trys xx = new Trys();
+            var xxx = xx.square(6);
+            Console.WriteLine(xxx);
+        }
+    }
+}
